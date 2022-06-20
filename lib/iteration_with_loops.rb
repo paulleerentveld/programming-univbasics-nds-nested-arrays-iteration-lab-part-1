f544a96c9bd9b224e4aef7c1c6fac96d4bd7fc2c
paulleerentveld <paul@veldnet.com.au>
2022-06-20 17:02:28 +1000

    lab done

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,4 +3,18 @@ def find_even_values(src)
   #
   # Output all even values in each nested array
 
+  outerCount = 0
+  evenNumbers = []
+
+  while outerCount < src.count do
+    innerCount = 0
+    while innerCount < src[outerCount].count do
+      p src[outerCount][innerCount] if src[outerCount][innerCount].even?
+    innerCount += 1
+    end
+    outerCount += 1
+  end
+
+  evenNumbers
+  
 end
